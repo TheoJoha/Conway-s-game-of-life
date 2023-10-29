176ff9c22dce573db410c6a0bc45b428b22fcaf8
Theodor Johansson <t.j.u@telia.com>
2023-10-29 16:48:12 +0100

    Finished a basic version of the game

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ let grid = [["o", "o", "o", "o", "o", "o", "o", "o"],
 ["o", "o", "o", "o", "o", "o", "o", "o"],
 ["o", "o", "o", "o", "o", "o", "o", "o"]]
 
-console.log(grid)
-console.log(grid.length)
 
 // set grid initially
 for (let i = 0; i < grid.length; i++) {
@@ -21,8 +19,6 @@ for (let i = 0; i < grid.length; i++) {
         }
     }
 }
-
-console.log(grid)
 
 // intermediary grid
 let intermediaryGrid = [[0, 0, 0, 0, 0, 0, 0, 0],
@@ -43,97 +39,110 @@ for (let i = 0; i < grid.length; i++) {
     }
 }
 
-// check which cells should live or die in the next generation
-for (let i = 0; i < grid.length; i++) {
+let turns = 0
+while (turns < 5) {
+    turns += 1
+    // check which cells should live or die in the next generation
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid.length; j++) {
+            let aliveCount = 0
+            // if i = j = 0
+            if (i == 0 && j == 0) {
+                if (grid[i][j + 1] == "o") aliveCount += 1
+                if (grid[i + 1][j] == "o") aliveCount += 1
+                if (grid[i + 1][j + 1] == "o") aliveCount += 1
+            }
+            // if i = 0 and 0 < j < grid.length - 1
+            else if (i == 0 && j > 0 && j < grid.length - 1) {
+                if (grid[i][j - 1] == "o") aliveCount += 1
+                if (grid[i][j + 1] == "o") aliveCount += 1
+                if (grid[i + 1][j - 1] == "o") aliveCount += 1
+                if (grid[i + 1][j] == "o") aliveCount += 1
+                if (grid[i + 1][j + 1] == "o") aliveCount += 1
+            }
+            // if i = 0 and j = grid.length - 1
+            else if (i == 0 && j == grid.length - 1) {
+                if (grid[i][j - 1] == "o") aliveCount += 1
+                if (grid[i + 1][j - 1] == "o") aliveCount += 1
+                if (grid[i + 1][j] == "o") aliveCount += 1
 
-    for (let j = 0; j < grid.length; j++) {
-        let aliveCount = 0
-        // if i = j = 0
-        if (i == 0 && j == 0) {
-            if (grid[i][j + 1] == "o") aliveCount += 1
-            if (grid[i + 1][j] == "o") aliveCount += 1
-            if (grid[i + 1][j + 1] == "o") aliveCount += 1
+            }
+            // if 0 < i < grid.length - 1 and j = 0
+            else if (i > 0 && i < grid.length - 1 && j == 0) {
+                if (grid[i - 1][j + 1] == "o") aliveCount += 1
+                if (grid[i - 1][j - 1] == "o") aliveCount += 1
+                if (grid[i][j + 1] == "o") aliveCount += 1
+                if (grid[i + 1][j] == "o") aliveCount += 1
+                if (grid[i + 1][j + 1] == "o") aliveCount += 1
+            }
+            // if 0 < i < grid.length - 1 and j = grid.length - 1
+            else if (i > 0 && i < grid.length - 1 && j == grid.length - 1) {
+                if (grid[i - 1][j] == "o") aliveCount += 1
+                if (grid[i - 1][j + 1] == "o") aliveCount += 1
+                if (grid[i][j - 1] == "o") aliveCount += 1
+                if (grid[i + 1][j - 1] == "o") aliveCount += 1
+                if (grid[i + 1][j] == "o") aliveCount += 1
+            }
+            // if i = grid.length - 1 and j = 0
+            else if (i == grid.length - 1 && j == 0) {
+                if (grid[i - 1][j + 1] == "o") aliveCount += 1
+                if (grid[i - 1][j - 1] == "o") aliveCount += 1
+                if (grid[i][j + 1] == "o") aliveCount += 1
+            }
+            // if i = grid.length - 1 and 0 < j < grid.length - 1
+            else if (i == grid.length - 1 && j > 0 && j < grid.length - 1) {
+                if (grid[i - 1][j] == "o") aliveCount += 1
+                if (grid[i - 1][j + 1] == "o") aliveCount += 1
+                if (grid[i - 1][j - 1] == "o") aliveCount += 1
+                if (grid[i][j - 1] == "o") aliveCount += 1
+                if (grid[i][j + 1] == "o") aliveCount += 1
+            }
+            // if i = grid.length - 1 and j = grid.length - 1
+            else if (i == grid.length - 1 && j == grid.length - 1) {
+                if (grid[i - 1][j] == "o") aliveCount += 1
+                if (grid[i - 1][j + 1] == "o") aliveCount += 1
+                if (grid[i][j - 1] == "o") aliveCount += 1
+            }
+            // else
+            else {
+                if (grid[i - 1][j] == "o") aliveCount += 1
+                if (grid[i - 1][j + 1] == "o") aliveCount += 1
+                if (grid[i - 1][j - 1] == "o") aliveCount += 1
+                if (grid[i][j - 1] == "o") aliveCount += 1
+                if (grid[i][j + 1] == "o") aliveCount += 1
+                if (grid[i + 1][j - 1] == "o") aliveCount += 1
+                if (grid[i + 1][j] == "o") aliveCount += 1
+                if (grid[i + 1][j + 1] == "o") aliveCount += 1
+            }
+            intermediaryGrid[i][j] = aliveCount
         }
-        // if i = 0 and 0 < j < grid.length - 1
-        else if (i == 0 && j > 0 && j < grid.length - 1) {
-            if (grid[i][j - 1] == "o") aliveCount += 1
-            if (grid[i][j + 1] == "o") aliveCount += 1
-            if (grid[i + 1][j - 1] == "o") aliveCount += 1
-            if (grid[i + 1][j] == "o") aliveCount += 1
-            if (grid[i + 1][j + 1] == "o") aliveCount += 1
-        }
-        // if i = 0 and j = grid.length - 1
-        else if (i == 0 && j == grid.length - 1) {
-            if (grid[i][j - 1] == "o") aliveCount += 1
-            if (grid[i + 1][j - 1] == "o") aliveCount += 1
-            if (grid[i + 1][j] == "o") aliveCount += 1
-
-        }
-        // if 0 < i < grid.length - 1 and j = 0
-        else if (i > 0 && i < grid.length - 1 && j == 0) {
-            if (grid[i - 1][j + 1] == "o") aliveCount += 1
-            if (grid[i - 1][j - 1] == "o") aliveCount += 1
-            if (grid[i][j + 1] == "o") aliveCount += 1
-            if (grid[i + 1][j] == "o") aliveCount += 1
-            if (grid[i + 1][j + 1] == "o") aliveCount += 1
-        }
-        // if 0 < i < grid.length - 1 and j = grid.length - 1
-        else if (i > 0 && i < grid.length - 1 && j == grid.length - 1) {
-            if (grid[i - 1][j] == "o") aliveCount += 1
-            if (grid[i - 1][j + 1] == "o") aliveCount += 1
-            if (grid[i][j - 1] == "o") aliveCount += 1
-            if (grid[i + 1][j - 1] == "o") aliveCount += 1
-            if (grid[i + 1][j] == "o") aliveCount += 1
-        }
-        // if i = grid.length - 1 and j = 0
-        else if (i == grid.length - 1 && j == 0) {
-            if (grid[i - 1][j + 1] == "o") aliveCount += 1
-            if (grid[i - 1][j - 1] == "o") aliveCount += 1
-            if (grid[i][j + 1] == "o") aliveCount += 1
-        }
-        // if i = grid.length - 1 and 0 < j < grid.length - 1
-        else if (i == grid.length - 1 && j > 0 && j < grid.length - 1) {
-            if (grid[i - 1][j] == "o") aliveCount += 1
-            if (grid[i - 1][j + 1] == "o") aliveCount += 1
-            if (grid[i - 1][j - 1] == "o") aliveCount += 1
-            if (grid[i][j - 1] == "o") aliveCount += 1
-            if (grid[i][j + 1] == "o") aliveCount += 1
-        }
-        // if i = grid.length - 1 and j = grid.length - 1
-        else if (i == grid.length - 1 && j == grid.length - 1) {
-            if (grid[i - 1][j] == "o") aliveCount += 1
-            if (grid[i - 1][j + 1] == "o") aliveCount += 1
-            if (grid[i][j - 1] == "o") aliveCount += 1
-        }
-        // else
-        else {
-            if (grid[i - 1][j] == "o") aliveCount += 1
-            if (grid[i - 1][j + 1] == "o") aliveCount += 1
-            if (grid[i - 1][j - 1] == "o") aliveCount += 1
-            if (grid[i][j - 1] == "o") aliveCount += 1
-            if (grid[i][j + 1] == "o") aliveCount += 1
-            if (grid[i + 1][j - 1] == "o") aliveCount += 1
-            if (grid[i + 1][j] == "o") aliveCount += 1
-            if (grid[i + 1][j + 1] == "o") aliveCount += 1
-        }
-        intermediaryGrid[i][j] = aliveCount
     }
+
+
+    // populate next generation of grid
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid.length; j++) {
+            if (grid[i][j] == "o" && intermediaryGrid[i][j] < 2) {
+                grid[i][j] = "x"
+            }
+            else if (grid[i][j] == "o" && intermediaryGrid[i][j] > 3) {
+                grid[i][j] = "x"
+            }
+            else if (grid[i][j] == "x" && intermediaryGrid[i][j] == 3)
+                grid[i][j] = "o"
+        }
+    }
+
+    let templateLittoralstr = ''
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid.length; j++) {
+            templateLittoralstr += `${grid[i][j]}`
+        }
+        templateLittoralstr += "\n"
+    }
+    console.log(templateLittoralstr)
+
+
 }
 
-console.log(intermediaryGrid)
 
-// populate next generation of grid
-for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid.length; j++) {
-        if (grid[i][j] == "o" && intermediaryGrid[i][j] < 2) {
-            grid[i][j] = "x"
-        }
-        else if (grid[i][j] == "o" && intermediaryGrid[i][j] > 3) {
-            grid[i][j] = "x"
-        }
-        else if (grid[i][j] == "x" && intermediaryGrid[i][j] == 3)
-            grid[i][j] = "o"
-    }
-}
-
-console.log(grid)
